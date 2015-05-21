1d74dd3ef0a27353227808a7fbb0a64e5356f65a
spenserhuang <spenserhuang515@gmail.com>
2015-05-20 21:08:32 -0700

    New Controller Pseudocode

---
+++ app/controllers/index.rb
@@ -0,0 +1,25 @@
+# homepage
+  # cities list
+  # post new room
+  # get
+  # post
+# open user page
+  # user's name
+  # list of rooms under that user
+  # get
+# self user page
+  # for updating user info
+  # get
+  # put
+# room page
+  # for displaying room info
+  # *** reserving the room
+  # get
+# room list
+  # list of all the rooms in that city
+  # get
+  # post
+# new room form
+  # form for creating a new room
+  # get
+  # post
